b9e8fefe7b226855b716e6bfccb25ceb1a188ec4
Christoph Girstenbrei <christoph.girstenbrei@gmail.com>
2018-02-26 13:13:08 +0100

    Adding thanks page

--- signup/urls.py
+++ signup/urls.py
@@ -4,5 +4,6 @@ from signup import views
 
 urlpatterns = [
     path('', views.add_participant),
+    path('thanks-<str:name>', views.thanks, name='thanks'),
     path('<slug:slug>', views.add_participant),
 ]
--- signup/views.py
+++ signup/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from signup.forms import EditParticipant
 from signup.models import Participant
@@ -13,8 +14,13 @@ def add_participant(request, slug=None):
         form = EditParticipant(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect('/')
+            # redirect_url = urlencode('/thanks-%s'.format(form.cleaned_data['forename']))
+            return HttpResponseRedirect(reverse('thanks', kwargs={'name': form.cleaned_data['forename']}))
     else:
         form = EditParticipant()
 
-    return render(request, 'events/edit_event.html', {'form': form})
+    return render(request, 'signup.html', {'form': form})
+
+
+def thanks(request, name):
+    return render(request, 'thanks.html', {'name': name})
