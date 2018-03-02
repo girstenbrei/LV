188ea4795d67223287ce70d6c63b354279143169
Christoph Girstenbrei <christoph.girstenbrei@gmail.com>
2018-03-02 16:38:57 +0100

    Mid switching to createview

--- events/urls.py
+++ events/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from events.views import EventList, EventDetail, EventView
 
 urlpatterns = [
-    path('', EventView.as_view(), name='event'),
-    path('list/', EventList.as_view(template_name='event_list.html'), name='eventlist'),
+    path('add/', EventView.as_view(), name='event_add'),
+    path('edit/<int:pk>', EventView.as_view(), name='event_edit'),
+    path('list/', EventList.as_view(template_name='event_list.html'), name='event_list'),
     path('<slug:slug>', EventDetail.as_view(), name='event_detail'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,24 +1,21 @@
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, FormView
+from django.views.generic import ListView, DetailView, CreateView
 
 from events.forms import EditEvent
 from events.models import Event
 
 
-class EventView(FormView):
-    template_name = 'events/edit_event.html'
+class EventView(CreateView):
+    model = Event
     form_class = EditEvent
-    success_url = reverse_lazy('eventlist')
-
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
+    success_url = reverse_lazy('event_list')
 
 
 class EventList(ListView):
     model = Event
     context_object_name = 'events'
+
 
 class EventDetail(DetailView):
     model = Event
