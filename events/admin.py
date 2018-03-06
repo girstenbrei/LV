2142daa648817a600d04712a982ffdaca259e295
Christoph Girstenbrei <christoph.girstenbrei@gmail.com>
2018-03-06 13:45:38 +0100

    Registering new models with admin

--- events/admin.py
+++ events/admin.py
@@ -1,10 +1,15 @@
 # Register your models here.
 from django.contrib import admin
 
-from events.models import Event, Question, Answer, QuestionSet, SignUp
+from events.models import Event, Question, QuestionSet, SignUp, CharAnswer, TextAnswer, DateAnswer, TimeAnswer, \
+    MailAnswer
 
 admin.site.register(Event)
 admin.site.register(SignUp)
 admin.site.register(QuestionSet)
 admin.site.register(Question)
-admin.site.register(Answer)
+admin.site.register(CharAnswer)
+admin.site.register(TextAnswer)
+admin.site.register(DateAnswer)
+admin.site.register(TimeAnswer)
+admin.site.register(MailAnswer)
